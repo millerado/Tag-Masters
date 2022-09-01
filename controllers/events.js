5faa5928d320f9a6539e312c7dc0d3e7b948d760
Seth Miller <millerado@gmail.com>
2022-09-01 11:17:03 -0600

    test basic routes

--- controllers/events.js
+++ controllers/events.js
@@ -3,3 +3,30 @@ const router = express.Router();
 const Book = require('../models/event');
 
 // INDUCES
+// Index
+router.get('/', (req, res) => {
+  res.send('index');
+});
+
+// New
+router.get('/new', (req, res) => {
+  res.send('index');
+});
+
+// Delete
+
+// Update
+
+// Create
+
+// Edit
+router.get('/:id/edit', (req, res) => {
+  res.send('index');
+});
+
+// Show
+router.get('/:id', (req, res) => {
+  res.send('index');
+});
+
+module.exports = router;
