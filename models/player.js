f36074ccebb56ad5f3471ebc3727890b22de30fa
Seth Miller <millerado@gmail.com>
2022-09-06 07:31:27 -0600

    added curentTag field with default

--- models/player.js
+++ models/player.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const playerSchema = new Schema({
   name: { type: String, required: true },
+  currentTag: { type: Number, default: 1000 },
 });
 
 module.exports = mongoose.model('Player', playerSchema);
