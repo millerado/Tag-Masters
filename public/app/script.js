ff6e5e5ee22b748300c18a5f81c21ffa3b38ae80
Seth Miller <millerado@gmail.com>
2022-09-06 20:50:23 -0600

    change sortTable function to sort scores

--- public/app/script.js
+++ public/app/script.js
@@ -16,15 +16,14 @@ $score.on('click', enterScore);
 // Functions
 function enterScore(event) {
   const score = prompt('Please enter score');
-  console.log(event.target);
   event.target.innerHTML = score;
 }
 
 function sortTable() {
   $rows.sort(function (a, b) {
-    console.log($(a).children('td').eq(3)[0]);
-    var A = parseInt($(a).children('td').eq(1).text(), 10);
-    var B = parseInt($(b).children('td').eq(1).text(), 10);
+    console.log($(a).children('td').eq(3).text());
+    let A = parseInt($(a).children('td').eq(3).text(), 10);
+    let B = parseInt($(b).children('td').eq(3).text(), 10);
     if (A < B) {
       return -1;
     }
