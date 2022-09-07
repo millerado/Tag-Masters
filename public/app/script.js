88ef2ed9232579f617f66f7bb893028fed6b0d11
Seth Miller <millerado@gmail.com>
2022-09-06 19:28:53 -0600

    make score clickable and add function to edit score

--- public/app/script.js
+++ public/app/script.js
@@ -7,13 +7,22 @@ const $button = $('#clickMe');
 const $table = $('table');
 const $rows = $('#mytable tbody tr');
 const $tbody = $('tbody');
+const $score = $('.score-cell');
 
 // Event Listeners
 $button.on('click', sortTable);
+$score.on('click', enterScore);
 
 // Functions
+function enterScore(event) {
+  const score = prompt('Please enter score');
+  console.log(event.target);
+  event.target.innerHTML = score;
+}
+
 function sortTable() {
   $rows.sort(function (a, b) {
+    console.log($(a).children('td').eq(3)[0]);
     var A = parseInt($(a).children('td').eq(1).text(), 10);
     var B = parseInt($(b).children('td').eq(1).text(), 10);
     if (A < B) {
