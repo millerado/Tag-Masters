bc14205e21c5934847b40bdfabea03f947ae3c95
Seth Miller <millerado@gmail.com>
2022-09-09 10:45:58 -0600

    change date field type to String

--- models/event.js
+++ models/event.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const eventSchema = new Schema(
   {
     name: { type: String, required: true },
-    date: { type: Date, required: true },
+    date: { type: String, required: true },
     course: { type: String, required: true },
     players: { type: Array },
     scores: { type: Array, default: [] },
